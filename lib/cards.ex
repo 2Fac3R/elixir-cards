7bc870b3cbcf882d235eca11708c00af88b3d75e
Gustavo Rodriguez <genr1818@gmail.com>
2024-10-16 18:35:25 -0600

    tests

--- lib/cards.ex
+++ lib/cards.ex
@@ -9,7 +9,7 @@ defmodule Cards do
   ## Examples
 
       iex> Cards.hello()
-      :world
+      "hello world"
 
   """
   def hello do
@@ -24,10 +24,13 @@ defmodule Cards do
 
   ## Examples
 
-    iex> Cards.create_deck()
-    [{"2", "hearts"}, {"3", "hearts"}, {"4", "hearts"}, ...]
+      iex> length(Cards.create_deck()) == 52
+      true
+
+      iex> Enum.uniq(Cards.create_deck()) |> length == 52  # Verifica que no haya duplicados
+      true
   """
-  def create_deck() do
+  def create_deck do
     suits = ["hearts", "diamonds", "clubs", "spades"]
     values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
     for suit <- suits, value <- values, do: {value, suit}
@@ -39,8 +42,11 @@ defmodule Cards do
   ## Examples
 
       iex> deck = [1, 2, 3, 4, 5]
-      iex> Cards.shuffle(deck)
-      [3, 2, 5, 1, 4]
+      iex> shuffled_deck = Cards.shuffle(deck)
+      iex> length(shuffled_deck) == length(deck)
+      true
+      iex> Enum.all?(deck, fn x -> Enum.member?(shuffled_deck, x) end)
+      true
 
   """
   def shuffle(deck) do
@@ -73,5 +79,53 @@ defmodule Cards do
   """
   def deal(deck, n) do
     Enum.split(deck, n)
+  end
+
+  @doc """
+  Converts a tuple into a string.
+
+  ## Parameters
+
+    - `tuple`: A tuple containing two elements, `key` and `value`.
+
+  ## Returns
+
+    - A string representation of the tuple in the format "key value".
+
+  ## Examples
+
+      iex> tuple_to_string({:name, "Alice"})
+      "name Alice"
+
+      iex> tuple_to_string({:age, 30})
+      "age 30"
+  """
+  def tuple_to_string({key, value}) do
+    "#{key} #{value}"
+  end
+
+  @doc """
+  Saves the deck to a file.
+
+  ## Parameters
+
+    - `deck`: A list of tuples representing the deck of cards.
+    - `filename`: The name of the file to save the deck to.
+
+  ## Returns
+
+      - `:ok` if the deck was saved successfully.
+      - `:error` if there was an error saving the deck.
+
+  ## Examples
+
+      iex> deck = Cards.create_deck()
+      iex> Cards.save(deck, "deck.txt")
+      :ok
+  """
+  def save(deck, filename \\ "deck.txt") do
+    content = deck |> Enum.map(&tuple_to_string/1) |> Enum.join("\n")
+    {:ok, :err} = File.write!(filename, content)
+    :ok
   end
 end
--- test/cards_test.exs
+++ test/cards_test.exs
@@ -1,8 +1,42 @@
 defmodule CardsTest do
   use ExUnit.Case
-  doctest Cards
 
-  test "greets the world" do
-    assert Cards.hello() == :world
+  describe "hello/0" do
+    test "returns 'hello world'" do
+      assert Cards.hello() == "hello world"
+    end
+  end
+
+  describe "create_deck/0" do
+    test "returns a deck of 52 unique cards" do
+      deck = Cards.create_deck()
+      assert length(deck) == 52
+      assert Enum.uniq(deck) |> length() == 52
+    end
+  end
+
+  describe "shuffle/1" do
+    test "shuffles the deck" do
+      deck = [1, 2, 3, 4, 5]
+      shuffled_deck = Cards.shuffle(deck)
+      assert length(shuffled_deck) == length(deck)
+      assert Enum.all?(deck, fn x -> Enum.member?(shuffled_deck, x) end)
+    end
+  end
+
+  describe "contains?/2" do
+    test "checks if a card is present in the deck" do
+      deck = [:ace, :king, :queen, :jack]
+      assert Cards.contains?(deck, :queen)
+    end
+  end
+
+  describe "deal/2" do
+    test "deals n cards from the deck" do
+      deck = [1, 2, 3, 4, 5, 6]
+      {hand, remaining} = Cards.deal(deck, 3)
+      assert hand == [1, 2, 3]
+      assert remaining == [4, 5, 6]
+    end
   end
 end
